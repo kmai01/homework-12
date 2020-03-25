70d8a4d580b88457d1b659ee021878fd7066f0b0
Khanh Mai <57655821+kmai01@users.noreply.github.com>
2020-03-25 16:17:12 -0500

    set up employee table

--- scheme.sql
+++ scheme.sql
@@ -2,3 +2,14 @@ DROP DATABASE IF EXISTS employeeDB;
 CREATE DATABASE employeeDB;
 
 USE employeeDB;
+
+CREATE TABLE employee (
+ id INT NOT NULL auto_increment,
+ first_name varchar(30) NULL,
+ last_name varchar(30) null,
+ role_id INT FOREIGN KEY,
+ manager_id INT FOREIGN key ,
+
+PRIMARY KEY (id),
+
+)};
