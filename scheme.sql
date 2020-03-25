d5ddbea6f68a3171c76bc9f002ecd4e33df14644
Khanh Mai <57655821+kmai01@users.noreply.github.com>
2020-03-25 16:32:41 -0500

    add more table to schema

--- scheme.sql
+++ scheme.sql
@@ -10,6 +10,21 @@ CREATE TABLE employee (
  role_id INT FOREIGN KEY,
  manager_id INT FOREIGN key ,
 
-PRIMARY KEY (id),
+PRIMARY KEY (id)
 
-)};
+);
+
+CREATE TABLE role (
+     id INT NOT NULL auto_increment,
+     title VARCHAR(30) NULL,
+     salary VARCHAR(30) NULL,
+     department_id INT FOREIGN KEY
+    PRIMARY KEY (id)
+    
+    );
+
+CREATE TABLE department (
+    id INT NOT NULL auto_increment,
+    name VARCHAR(30)
+    PRIMARY KEY (id)
+);
