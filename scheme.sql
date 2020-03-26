2758713892e454ae914259f6c81065c6094c7193
Khanh Mai <57655821+kmai01@users.noreply.github.com>
2020-03-26 03:25:45 -0500

    no error in schema

--- scheme.sql
+++ scheme.sql
@@ -3,28 +3,35 @@ CREATE DATABASE employeeDB;
 
 USE employeeDB;
 
-CREATE TABLE employee (
- id INT NOT NULL auto_increment,
- first_name varchar(30) NULL,
- last_name varchar(30) null,
- role_id INT FOREIGN KEY,
- manager_id INT FOREIGN key ,
-
-PRIMARY KEY (id)
-
+CREATE TABLE department (
+    Id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (Id)
 );
 
-CREATE TABLE role (
-     id INT NOT NULL auto_increment,
+CREATE TABLE roles (
+     Id INT NOT NULL AUTO_INCREMENT,
      title VARCHAR(30) NULL,
      salary VARCHAR(30) NULL,
-     department_id INT FOREIGN KEY
+     Department_ID INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id),
     PRIMARY KEY (id)
     
     );
 
-CREATE TABLE department (
-    id INT NOT NULL auto_increment,
-    name VARCHAR(30)
-    PRIMARY KEY (id)
+CREATE TABLE employee (
+
+ Id INT NOT NULL AUTO_INCREMENT,
+ first_name varchar(30) NULL,
+ last_name varchar(30) NULL,
+ Role_ID INT NOT NULL,
+ FOREIGN KEY(Role_Id) REFERENCES roles(Id),
+ Manager_ID INT NOT NULL,
+ FOREIGN KEY(Manager_ID) REFERENCES department(Id),
+PRIMARY KEY(Id)
+
 );
+
+
+
+
